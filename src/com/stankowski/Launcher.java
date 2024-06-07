093a66d627bada93d1337cac6fe2915f01238af1
Mateusz Stankowski <stankowski1992@gmail.com>
2024-06-07 16:14:24 +0200

    Initial commit

---
+++ src/com/stankowski/Launcher.java
@@ -0,0 +1,27 @@
+package com.stankowski;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent parent = FXMLLoader.load(getClass().getResource("view/first.fxml"));
+
+        Scene scene = new Scene(parent, 300, 250);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module EmailClientJavaFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.stankowski;
+    opens com.stankowski.view;
+}
