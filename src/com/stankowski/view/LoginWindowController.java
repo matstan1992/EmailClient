0914fc70eda5769e3d25a96883be60dc9f0a0e32
Mateusz Stankowski <stankowski1992@gmail.com>
2024-06-08 21:31:10 +0200

    Created login window layout

---
+++ src/com/stankowski/view/LoginWindowController.java
@@ -0,0 +1,23 @@
+package com.stankowski.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("Click!");
+    }
+}
