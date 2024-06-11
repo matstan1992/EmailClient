8bb5bcb50766d8bfaff97c69849bcf4cc8b35711
Mateusz Stankowski <stankowski1992@gmail.com>
2024-06-11 09:50:10 +0200

    Initialize stages

--- src/com/stankowski/view/ViewFactory.java
+++ src/com/stankowski/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.stankowski.view;
 
 import com.stankowski.EmailManager;
+import com.stankowski.controller.BaseController;
+import com.stankowski.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -12,5 +20,21 @@ public class ViewFactory {
 
     public void showLoginWindow() {
         System.out.println("show window login");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
