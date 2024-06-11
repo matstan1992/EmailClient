b4c3d5b1c8caf43e3506aa4ae10e6bcc203b1911
Mateusz Stankowski <stankowski1992@gmail.com>
2024-06-11 12:36:37 +0200

    Added change from login window to main window after button click

--- src/com/stankowski/view/ViewFactory.java
+++ src/com/stankowski/view/ViewFactory.java
@@ -3,6 +3,7 @@ package com.stankowski.view;
 import com.stankowski.EmailManager;
 import com.stankowski.controller.BaseController;
 import com.stankowski.controller.LoginWindowController;
+import com.stankowski.controller.MainWindowController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -22,8 +23,19 @@ public class ViewFactory {
         System.out.println("show window login");
 
         BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
-        fxmlLoader.setController(controller);
+        initializeStage(controller);
+    }
+
+    public void showMainWindow() {
+        System.out.println("main window called");
+
+        BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    public void initializeStage(BaseController baseController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
         Parent parent;
         try {
             parent = fxmlLoader.load();
@@ -36,5 +48,9 @@ public class ViewFactory {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void closeStage(Stage stageToClose) {
+        stageToClose.close();
     }
 }
