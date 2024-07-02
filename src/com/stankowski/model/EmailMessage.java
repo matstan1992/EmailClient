a74d257ba203c3ba559515e0c311a3527713020a
Mateusz Stankowski <stankowski1992@gmail.com>
2024-07-02 12:53:45 +0200

    Getting attachments

--- src/com/stankowski/model/EmailMessage.java
+++ src/com/stankowski/model/EmailMessage.java
@@ -6,7 +6,11 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.css.Size;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class EmailMessage {
     private SimpleStringProperty subject;
@@ -16,6 +20,8 @@ public class EmailMessage {
     private SimpleObjectProperty<Date> date;
     private boolean isRead;
     private Message message;
+    private List<MimeBodyPart> attachmentList = new ArrayList<MimeBodyPart>();
+    private boolean hasAttachments = false;
 
     public EmailMessage(String subject, String sender, String recipient, int size, Date date, boolean isRead,Message message) {
         this.subject = new SimpleStringProperty(subject);
@@ -50,5 +56,15 @@ public class EmailMessage {
     }
     public Message getMessage() {
         return this.message;
+    }
+
+    public void addAttachment(MimeBodyPart mbp) {
+        hasAttachments = true;
+        attachmentList.add(mbp);
+        try {
+            System.out.println("Added attach: " + mbp.getFileName());
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
     }
 }
