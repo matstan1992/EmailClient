540ad5d16edb2957e1bbc6c3f8fe194aa850ea75
Mateusz Stankowski <stankowski1992@gmail.com>
2024-07-02 12:04:18 +0200

    Handling attachments with multipart

--- src/com/stankowski/controller/services/MessageRendererService.java
+++ src/com/stankowski/controller/services/MessageRendererService.java
@@ -56,20 +56,26 @@ public class MessageRendererService extends Service {
             stringBuffer.append(message.getContent().toString());
         } else if(isMultipartType(contentType)) {
             Multipart multipart = (Multipart) message.getContent();
-            for (int i = multipart.getCount() - 1; i >= 0; i--) {
-                BodyPart bodyPart = multipart.getBodyPart(i);
-                String bodyPartContentType = bodyPart.getContentType();
-                if(isSimpleType(bodyPartContentType)) {
-                    stringBuffer.append(bodyPart.getContent().toString());
-                }
+            loadMultipart(multipart, stringBuffer);
+        }
+    }
+
+    private void loadMultipart(Multipart multipart, StringBuffer stringBuffer) throws MessagingException, IOException {
+        for (int i = multipart.getCount() - 1; i >= 0; i--) {
+            BodyPart bodyPart = multipart.getBodyPart(i);
+            String contentType = bodyPart.getContentType();
+            if(isSimpleType(contentType)) {
+                stringBuffer.append(bodyPart.getContent().toString());
+            } else if(isMultipartType(contentType)) {
+                Multipart multipart2 = (Multipart) bodyPart.getContent();
+                loadMultipart(multipart2, stringBuffer);
             }
         }
     }
 
     private boolean isSimpleType(String contentType) {
         if(contentType.contains("TEXT/HTML") ||
-        //contentType.contains("mixed") ||
-        contentType.contains("text")) {
+            contentType.contains("text")) {
             return true;
         } else {
             return false;
